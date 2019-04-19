2e94f9c1efc02dddd6e023a461dabc140ea69585
catherinism <catcreates13@gmail.com>
2019-04-19 18:47:09 -0400

    add new and create method

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,11 +1,18 @@
 class SessionsController < ApplicationController
 
+  #render log in form
   def new
+    @user = User.new
     render :login
   end
 
   def create
-
+    @user = User.find_by(username: params[:user][:username])
+    if @user && @user.authenticate(params[:user][:username])
+      session[:id] = @user.id
+      redirect_to guides_path
+    else
+      redirect_to '/login'
   end
 
   def home

d9c5e99443589a06b9305b001d28a74875c5f681
catherinism <catcreates13@gmail.com>
2019-04-21 19:38:57 -0400

    add methods to guides controller

--- app/controllers/guides_controller.rb
+++ app/controllers/guides_controller.rb
@@ -1,2 +1,25 @@
 class GuidesController < ApplicationController
+  def index
+  end
+
+  def new
+    @guide = Guide.new
+  end
+
+  def create
+  end
+
+  def show
+
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
 end

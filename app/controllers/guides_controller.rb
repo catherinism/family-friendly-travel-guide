3c8a13511b055fb8ca1eb01789a17f195a4f4d7b
catherinism <catcreates13@gmail.com>
2019-04-22 19:12:20 -0400

    add create method and before action

--- app/controllers/guides_controller.rb
+++ app/controllers/guides_controller.rb
@@ -1,4 +1,7 @@
 class GuidesController < ApplicationController
+
+  before_action :set_guide, only: [:show, :edit, :update, :destroy]
+
   def index
   end
 
@@ -7,10 +10,15 @@ class GuidesController < ApplicationController
   end
 
   def create
+    @guide = Guide.new
+    if @guide.save
+      redirect_to guide_path(@guide)
+    else
+      render :new
+    end
   end
 
   def show
-
   end
 
   def edit
@@ -20,6 +28,16 @@ class GuidesController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def guide_params
+    params.require(:guide).permit(:location, :summary)
+  end
+
+  def set_guide
+    @guide = Guide.find(params[:id])
   end
 
 end

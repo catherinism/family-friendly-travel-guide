46f90f9267f6407a186daf353ce8337326ed7808
catherinism <catcreates13@gmail.com>
2019-04-23 21:30:23 -0400

    add logout route

--- config/routes.rb
+++ config/routes.rb
@@ -5,11 +5,18 @@ Rails.application.routes.draw do
    resources :guides do
      resources :ratings, only: [:create]
    end
-    resources :users, only: [:new, :create, :show] do
+    resources :users, only: [:show] do
       resources :guides, only: [:index]
   end
 
-  resources :sessions, only: [:new, :create, :destroy]
+  get '/signup', to: 'users#new'
+  post '/signup', to: 'users#create'
+
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+
+  delete '/logout', to: 'sessions#destroy'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 end

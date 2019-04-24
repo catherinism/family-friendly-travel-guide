cf2c36894a0a196333e4bb30c8cfd60672d20987
catherinism <catcreates13@gmail.com>
2019-04-23 21:25:42 -0400

    fix create method

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,24 +2,22 @@ class SessionsController < ApplicationController
 
   #render log in form
   def new
-    @user = User.new
-    render :login
   end
 
   def create
-    @user = User.find_by(username: params[:user][:username])
-    if @user && @user.authenticate(params[:user][:username])
-      session[:id] = @user.id
+    @user = User.find_by(username: params[:username])
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
       redirect_to guides_path
     else
-      redirect_to 'login'
+      redirect_to login_path
     end
   end
 
 
   def destroy
     session.clear
-    redirect_to '/'
+    redirect_to root_path
   end
 
 end

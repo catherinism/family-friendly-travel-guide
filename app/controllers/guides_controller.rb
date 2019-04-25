11f04bc8201481772a19db6b850fcc8c0be29594
catherinism <catcreates13@gmail.com>
2019-04-24 21:21:58 -0400

    modify guides controller

--- app/controllers/guides_controller.rb
+++ app/controllers/guides_controller.rb
@@ -17,7 +17,7 @@ class GuidesController < ApplicationController
   end
 
   def create
-    @guide = Guide.new(guide_params)
+    @guide = Guide.create(guide_params)
     if @guide.save
       redirect_to guide_path(@guide)
     else

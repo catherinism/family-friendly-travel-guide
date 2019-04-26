eecfbcc2f7727e2947e2328f68388c1063387c9e
catherinism <catcreates13@gmail.com>
2019-04-25 21:39:00 -0400

    modify users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,8 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       session[:user_id] = @user.id
-      redirect_to user_guides_path(@user)
+      flash[:message] = "Successfully Signed Up!"
+      redirect_to guides_path
     else
       render :new
     end

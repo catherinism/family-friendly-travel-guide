dedfde1a3a225aa102dfcc1b4b14756b1fd252fa
catherinism <catcreates13@gmail.com>
2019-04-27 14:57:58 -0400

    add validations to models

--- app/models/guide.rb
+++ app/models/guide.rb
@@ -1,7 +1,11 @@
 class Guide < ApplicationRecord
   has_many :ratings
   has_many :users, through: :ratings
-  belongs_to :destination
+  belongs_to :user
+  belongs_to :destination, optional: true
 
+  validates :title, presence: true
+  validates :summary, presence: true
+  
   scope :ordered_by_ratings, -> { joins(:ratings)}
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,9 @@
 class User < ApplicationRecord
-  has_secure_password
   has_many :ratings
   has_many :guides, through: :ratings
+  has_secure_password
 
-  validates :username, presence: true
-  validates :username, uniqueness: true
+  validates :name, presence: true
+  validates :username, presence: true, uniqueness: true
+
 end

3a11f70daff741f7d0c7b513ccaf1b78a1066678
catherinism <catcreates13@gmail.com>
2019-04-28 11:43:24 -0400

    refactor methods and add flash messages

--- app/controllers/guides_controller.rb
+++ app/controllers/guides_controller.rb
@@ -1,15 +1,17 @@
+require 'pry'
+
 class GuidesController < ApplicationController
 
   before_action :set_guide, only: [:show, :edit, :update, :destroy]
-  before_action :redirect_if_not_authorized!, only: [:edit, :update, :destroy]
+  before_action :not_authorized!, only: [:edit, :update, :destroy]
 
   def index
-    # if params[:user_id]
-    #   @user = User.find_by(params[:user_id])
-    #   @guides = @user.guides
-    # else
-    #   @guides = Guide.all
-    # end
+    if params[:user_id]
+      @user = User.find_by(id: params[:user_id])
+      @guides = @user.guides
+    else
+      @guides = Guide.all
+    end
   end
 
   def new
@@ -18,8 +20,9 @@ class GuidesController < ApplicationController
 
   def create
     @guide = Guide.new(guide_params)
-    @guide.user_id = current_user.id
+    @guide.user = current_user
     if @guide.save
+      flash[:message] = "#{@guide.title} was successfully added."
       redirect_to guide_path(@guide)
     else
       render :new
@@ -27,7 +30,8 @@ class GuidesController < ApplicationController
   end
 
   def show
-    @guide = Guide.find(params[:id])
+    @rating = Rating.new
+  end
   end
 
   def edit
@@ -35,19 +39,15 @@ class GuidesController < ApplicationController
 
   def update
     if @guide.update(guide_params)
-      flash[:message] = "Successfully edited."
+      flash[:message] = "#{@guide.title} was successfully edited."
       redirect_to guide_path(@guide)
-    else
-      render :edit
     end
   end
 
   def destroy
     if @guide.destroy
-      flash[:message] = "Successfully deleted."
+      flash[:message] = "#{@guide.title} was successfully deleted."
       redirect_to guides_path
-    else
-      render :edit
     end
   end
 
@@ -58,11 +58,12 @@ class GuidesController < ApplicationController
   end
 
   def set_guide
-    @guide = Guide.find(params[:id])
+    @guide = Guide.find_by(id: params[:id])
   end
 
-  def redirect_if_not_authorized!
-    if @guide.user_id != current_user.id
+  def not_authorized!
+    if @guide.user != current_user
+      flash[:message] = "You can only make changes to your own guide."
       redirect_to '/guides'
     end
   end

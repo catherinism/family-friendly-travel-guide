640b84698e3f965d0b189288f69d3444b370767c
catherinism <catcreates13@gmail.com>
2019-05-02 21:17:18 -0400

    add methods to ratings controller

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -1,2 +1,50 @@
 class RatingsController < ApplicationController
+  before_action :find_guide
+  before_action :find_rating, only: [:edit, :update, :destroy]
+
+  def new
+    @rating =Rating.new
+  end
+
+  def create
+    @rating = Rating.new(rating_params)
+    @rating.guide_id = @guide.id
+    @rating.user_id = current_user.id
+
+    if @rating.save
+      redirect_to guide_path(@guide)
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @rating.update(review_params)
+      redirect_to guide_path(@guide)
+    else
+      render :edit
+  end
+
+  def destroy
+    @rating.destroy
+    redirect_to guide_path(@guide)
+  end
+
+  private
+
+    def rating_params
+      params.require(:rating).permit(:value)
+    end
+
+    def find_guide
+      @guide = Guide.find(params[:guide_id])
+    end
+
+    def find_rating
+      @rating = Rating.find(params[:id])
+    end
+
 end

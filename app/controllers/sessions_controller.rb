000abeed2e5eee0fa746f257dcea810523457ef3
catherinism <catcreates13@gmail.com>
2019-05-05 14:12:45 -0400

    add google omniauth method

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -18,9 +18,45 @@ class SessionsController < ApplicationController
     end
   end
 
+  # def fbcreate
+  #   @user = User.find_or_create_by(uid: auth['uid']) do |u|
+  #     u.username = auth['info']['name']
+  #     u.email = auth['info']['email']
+  #     u.password = auth['uid']
+  #   end
+  #   session[:user_id] = @user.id
+  #   redirect_to guides_path
+  # end
+
+
+  def create_from_omniauth
+    #binding.pry
+    @user = User.find_or_create_by(email: auth[:info][:email]) do |user|
+      user.username = auth[:info][:name]
+      user.password = SecureRandom.hex
+    end
+#binding.pry
+    session[:user_id] = @user.id
+
+
+    if logged_in?
+      flash[:message] = "Successfully authenticated via Google!"
+    else
+      flash[:message] = "Something went wrong. Try again."
+    end
+
+    redirect_to root_path
+  end
+
   def destroy
     session.clear
     redirect_to root_path
+  end
+
+  private
+
+  def auth
+    request.env['omniauth.auth']
   end
 
 end

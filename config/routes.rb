015a32970c317d2093b58f408b63a06fe98cd099
catherinism <catcreates13@gmail.com>
2019-05-05 14:15:55 -0400

    add google omniauth route

--- config/routes.rb
+++ config/routes.rb
@@ -23,7 +23,7 @@ Rails.application.routes.draw do
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
 
-  get '/auth/facebook/callback', to: 'sessions#create'
+  get '/auth/google_oauth2/callback', to: 'sessions#create_from_omniauth'
 
   delete '/logout', to: 'sessions#destroy'
 

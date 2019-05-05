026c552e01a8ae24294ae92117af636c26b3d4ea
catherinism <catcreates13@gmail.com>
2019-05-04 22:47:45 -0400

    modify some methods and add rated method

--- app/controllers/guides_controller.rb
+++ app/controllers/guides_controller.rb
@@ -6,35 +6,46 @@ class GuidesController < ApplicationController
   before_action :not_authorized!, only: [:edit, :update, :destroy]
 
   def index
-    if params[:destination].blank?
-      @guides = Guide.all
+    if !params[:destination_id].blank?
+      #binding.pry
+      @destination_id = Destination.find_by(id: params[:destination_id]).id
+      @guides = Guide.where(destination_id: @destination_id)
     else
-      @destination_id = Destination.find_by(id: params[:location_id]).id
-      @guides = Destination.where(destination_id: @destination_id)
+      @guides = Guide.all
     end
+  end
+
+  def rated
+    @guides = Guide.rated
+    render action: :index
   end
 
   def new
     @guide = current_user.guides.build
-    @destinations = Destination.all.map { |d| [d.location, d.id] }
+    #@guide.user_id = current_user.id
+    #@destinations = Destination.all.map { |d| [d.location, d.id] }
   end
 
   def create
     @guide = current_user.guides.build(guide_params)
-    @guide.destination_id = params[destination_id]
+    # @guide.destination_id = params[destination_id]
+    @guide.user_id = current_user.id
     if @guide.save
       flash[:message] = "#{@guide.title} was successfully added."
       redirect_to guide_path(@guide)
     else
+      flash[:message] = "#{@guide.errors.full_messages.to_sentence}."
       render :new
     end
   end
 
   def show
+    @user = User.find_by(id: params[:id])
       if !logged_in?
     #@rating = Rating.new
       flash[:message] = "Log in to view the details."
       redirect_to root_path
+
     end
   end
 
@@ -66,7 +77,7 @@ class GuidesController < ApplicationController
   end
 
   def not_authorized!
-    if @guide.user != current_user
+    if @guide.user_id != current_user.id
       flash[:message] = "You can only make changes to your own guide."
       redirect_to '/guides'
     end

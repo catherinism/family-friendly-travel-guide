e721fea4294bcc9430c7a82db0da3f977615586c
catherinism <catcreates13@gmail.com>
2019-05-06 19:03:26 -0400

    validates email instead of username

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,6 @@ class User < ApplicationRecord
   has_secure_password
 
   validates :name, presence: true
-  validates :username, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true
 
 end

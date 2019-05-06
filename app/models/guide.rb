da3e5e8d908a6cb038fcc63f433d670b94766a1b
catherinism <catcreates13@gmail.com>
2019-05-06 19:02:12 -0400

    define custom setter/getter for destination

--- app/models/guide.rb
+++ app/models/guide.rb
@@ -9,5 +9,12 @@ class Guide < ApplicationRecord
 
   scope :rated, -> { joins(:ratings).distinct("ratings.guide_id") }
 
+  def destination_location=(location)
+    self.destination = Destination.find_or_create_by(location: location)
+  end
+
+  def destination_location
+     self.destination ? self.destination.location : nil
+  end
 
 end
